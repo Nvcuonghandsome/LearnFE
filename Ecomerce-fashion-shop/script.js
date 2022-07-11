09d574a7d334020166f3bd4670dac8401eead377
unknown <nvcuongbk94@gmail.com>
2022-07-11 21:24:33 +0700

    Ecomerce about page

--- Ecomerce-fashion-shop/script.js
+++ Ecomerce-fashion-shop/script.js
@@ -1,3 +1,5 @@
+
+// click menu on header
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
@@ -13,3 +15,32 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+
+// click img shop
+const mainImg = document.getElementById('mainImg')
+const smallImg = document.getElementsByClassName('small-img')
+
+if (smallImg) {
+    for (let i = 0; i <= smallImg.length; i++) {
+        if (smallImg[i])
+            smallImg[i].onclick = () => {
+                mainImg.src = smallImg[i].src
+            }
+    }
+}
+
+// product click
+const products = document.getElementsByClassName('product')
+
+if (products) {
+    for (let i = 0; i <= products.length; i++) {
+        if (products[i])
+            products[i].onclick = () => {
+                window.location.href = 'sproduct.html'
+                // mainImg.src = products[i].firstElementChild.src
+                // smallImg[0].src = products[i].firstElementChild.src
+                // Ask Hoai why ?
+            }
+    }
+}
+
